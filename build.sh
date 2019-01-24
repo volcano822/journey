e8f63551ef9acfffa3a4a4cbfca07d12eda8db1a
hexuefei <hexuefei@daixiaomi.com>
2019-01-24 20:41:27 +0800

    update

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pwd=$(pwd)
+export GOPATH=$pwd/../../../..
+go install github.com/volcano822/journey
+
+rm -rf output
+mkdir output
+cp -r built-in output/
+cp -r content output/
+cp config.json output/
+cp $GOPATH/bin/journey output/
+cd output
+tar -czf journey.tar.gz *
